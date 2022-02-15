f25c6dc7bd2128a6579b293b1002f7398087d6f0
CreativeOZ <zagriaoleg@gmail.com>
2022-02-15 16:34:01 +0300

    Показать числа от -N до N.

---
+++ zadanie_7/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа от -N до N
+Console.Clear();
+
+Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine());
+int count = N;
+N = N-(N*2);
+while (N <= count)
+{
+    Console.WriteLine(N);
+    N = N + 1;
+}
